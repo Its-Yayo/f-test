c9c3553ee11ae822959ca1dfa73071f401129b17
Its-Yayo <a01754574@itesm.mx>
2023-09-12 16:24:24 -0600

    Add Enviroment .env TODO #2

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python3
 
 from flask import Flask
+from dotenv import load_dotenv
 import mariadb
 import sys
 import os
 
 app = Flask(__name__)
+load_dotenv()
+
+config = {
+    'host': os.getenv('DB_HOST'),
+    'port': int(os.getenv('DB_PORT')),
+    'user': os.getenv('DB_USERNAME'),
+    'password': os.getenv('DB_PASSWORD'),
+    'database': os.getenv('DB_NAME')
+}
+
+try:
+    conn = mariadb.connect(**config)
+    cursor = conn.cursor()
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
 
 @app.route("/")
 def main() -> str:
