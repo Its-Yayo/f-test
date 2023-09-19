162e67cb78fde334af36361316e06e6d2ad056de
Its-Yayo <a01754574@itesm.mx>
2023-09-19 13:41:44 -0600

    Add improvements listener index.js #37

--- static/index.js
+++ static/index.js
@@ -1,3 +1,12 @@
-function main() {
-    console.log('TODO Implement me!');
+const btndelete = document.querySelectorAll('.btn-delete')
+
+if (btndelete) {
+    const btndeleteArray = Array.from(btndelete);
+    btndeleteArray.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            if (!confirm('Are you sure you want to delete it?')) {
+                e.preventDefault();
+            }
+        });
+    });
 }
